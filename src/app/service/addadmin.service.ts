3671399502231871f2f1c8b0923114d5f8a306a2
Prashant Singh <pprashantsingh2002@gmail.com>
2023-12-02 18:57:01 +0530

    service component

---
+++ src/app/service/addadmin.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs";
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddadminService {
+
+  apiUrl=environment.host;
+
+  constructor(private http:HttpClient) { }
+
+  addAdmin(data:any):Observable<any>{
+    console.log(data,"123456789")
+    return this.http.post(`${this.apiUrl}/add/admin`,data)
+  }
+}
