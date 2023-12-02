b4e54a2fde301ede6a8515c84de4c77a94234b07
Prashant Singh <pprashantsingh2002@gmail.com>
2023-12-02 19:03:04 +0530

    commit all another modified files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,56 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { MainComponent } from './main/main.component';
+import { AddadminComponent } from './main/addadmin/addadmin.component';
+import { AddproductComponent } from './main/addproduct/addproduct.component';
+import { DashboardComponent } from './main/dashboard/dashboard.component';
+import { ViewproductComponent } from './main/viewproduct/viewproduct.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button';
+import { AuthInterceptor } from './loginAuth/auth.interceptor';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatInputModule} from '@angular/material/input';
+
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    SidebarComponent,
+    NotfoundComponent,
+    MainComponent,
+    AddadminComponent,
+    AddproductComponent,
+    DashboardComponent,
+    ViewproductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatStepperModule,
+    MatInputModule,
+    MatPaginatorModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass:AuthInterceptor,
+      multi:true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
